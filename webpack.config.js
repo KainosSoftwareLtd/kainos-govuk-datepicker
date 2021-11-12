4a02b8dd87a1bb4413e25d10f163e37598ec424b
James Nelson <Jamesnelson117@gmail.com>
2021-11-12 17:04:16 +0000

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const {CleanWebpackPlugin} = require("clean-webpack-plugin");
 const webpack = require("webpack");
 
 module.exports = {
+  mode: 'development',
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
