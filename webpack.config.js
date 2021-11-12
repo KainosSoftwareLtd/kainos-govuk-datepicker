fffdecd7f7232162f9afabd5b4fd839ecc852324
James Nelson <jamesnelson117@gmail.com>
2021-11-12 22:27:40 +0000

    refactor: update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
+    filename: 'date-picker.js',
   },
   optimization: {
     minimize: true,
