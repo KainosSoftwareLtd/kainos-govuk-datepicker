13f90f09b94eb6dff726358c82c0b639887f4223
James Nelson <Jamesnelson117@gmail.com>
2021-11-15 15:55:16 +0000

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const DIST_DIR = path.resolve(__dirname, './dist');
+const DIST_DIR = path.resolve(__dirname, './dist')
 
 app.use(express.static(DIST_DIR));
 
