3be9627b9bd5e8b486f3740bcbcbb4f7642e0303
James Nelson <Jamesnelson117@gmail.com>
2021-11-16 14:30:55 +0000

    fix: webpack outputs es5

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  target: 'web',
+  target: ['web', 'es5'],
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
