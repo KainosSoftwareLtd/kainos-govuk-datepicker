217bdec64743a4f62657974f6fdb10164b21b54c
lewisbirks <birkslewis@gmail.com>
2022-08-26 13:10:34 +0100

    refactor: make use of new functionality

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,9 +1,11 @@
-const { setHeadlessWhen } = require('@codeceptjs/configure');
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
 const server = require('./server');
 
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
+
+setCommonPlugins();
 
 exports.config = {
   tests: './src/__tests__/e2e/*.test.js',
@@ -23,16 +25,5 @@ exports.config = {
   },
   mocha: {},
   name: 'date-picker',
-  plugins: {
-    pauseOnFail: {},
-    retryFailedStep: {
-      enabled: true,
-    },
-    tryTo: {
-      enabled: true,
-    },
-    screenshotOnFail: {
-      enabled: true,
-    },
-  },
+  plugins: {},
 };
